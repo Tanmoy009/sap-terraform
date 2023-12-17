9d5ff2c9f31c8e12d90fb100b662f6203ce0e11b
Tanmoy009 <97807066+Tanmoy009@users.noreply.github.com>
2023-12-17 22:58:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,30 +18,47 @@ resource "aws_vpc" "sap-nprod-vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
+
+  tags = {
+    terraform = "true"
+    Name = "sap-nprod-vpc"
+  }
 }
 
 # Create private subnets in sap-nprod-vpc
 resource "aws_subnet" "private_subnet1a" {
-  name        = "private_subnet1a"
   vpc_id                  = aws_vpc.sap-nprod-vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
+
+  tags = {
+    terraform = "true"
+    Name = "private_subnet1a"
+  }
 }
 
 resource "aws_subnet" "private_subnet1b" {
-  name        = "private_subnet1b"
   vpc_id                  = aws_vpc.sap-nprod-vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
+
+  tags = {
+    terraform = "true"
+    Name = "private_subnet1b"
+  }
 }
 
 # Create security group for VPC1 private subnets
 resource "aws_security_group" "sg_sap-nprod-vpc_private" {
-  name        = "sg_sap-nprod-vpc_private"
   description = "Security Group for VPC1 Private Subnets"
   vpc_id      = aws_vpc.sap-nprod-vpc.id
+
+  tags = {
+    terraform = "true"
+    Name = "sg_sap-nprod-vpc_private"
+  }
 }
 
 # Allow inbound traffic on specific ports (adjust as needed)
@@ -56,8 +73,12 @@ resource "aws_security_group_rule" "sg_sap-nprod-vpc_private_inbound" {
 
 # Create route table for VPC1 private subnets
 resource "aws_route_table" "rt_sap-nprod-vpc_private" {
-  name        = "rt_sap-nprod-vpc_private"
   vpc_id = aws_vpc.sap-nprod-vpc.id
+
+  tags = {
+    terraform = "true"
+    Name = "rt_sap-nprod-vpc_private"
+  }
 }
 
 # Associate route table with VPC1 private subnets
